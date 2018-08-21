401cfe97fa5945dbc4056532a3713148d4268ef7
Ignacio R Espino <irespino@capozios.com>
2018-08-21 18:12:57 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -34,7 +34,7 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
-  for counter in number_of_times do
+  for counter in 1..number_of_times do
     puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
